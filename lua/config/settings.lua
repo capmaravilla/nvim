7a66dd9622df00494548b9101522a95155c685a7
David Moriez <capmaravilla@gmail.com>
2026-01-06 11:22:37 +0100

    Hemos instalado ftplugins

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,10 +1,15 @@
 vim.opt.number = true
-vim.opt.relativenumber = false
-vim.opt.cursorline = false
-vim.opt.wrap = true
-vim.opt.expandtab = false
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
-vim.opt.swapfile = false
--- vim.opt.termguicolors = true
+
+-- Este lo tenemos actualmente en lazy.lua
+--vim.g.mapleader = " " 
+
+--Otras configuraciones
+--vim.opt.cursorline = false
+--vim.opt.wrap = true
+--vim.opt.expandtab = false
+--vim.opt.tabstop = 4
+--vim.opt.shiftwidth = 4
+--vim.opt.swapfile = false
+--vim.opt.termguicolors = true
