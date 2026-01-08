17c2a664937745ae8f11d63634c4d8ca67dcbde2
David Moriez <capmaravilla@gmail.com>
2026-01-08 09:37:03 +0100

    Cambiada la barra de comandos con noice.nvim

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    lsp = { override = { "vim.notify" } },
+    cmdline = { view = "cmdline_popup" },
+    views = {
+      cmdline_popup = {
+        position = { row = "20%", col = "50%" },
+        size = { width = 60, height = "auto" },
+      },
+    },
+  },
+}
+
