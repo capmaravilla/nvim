d6002f3b512f5c54f9b11635d9dbdff0790b1879
David Moriez <capmaravilla@gmail.com>
2026-01-08 21:05:23 +0100

    cambio el overlay de which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,7 +1,35 @@
+-- return {
+-- 	"folke/which-key.nvim",
+-- 	event = "VeryLazy",
+-- 	config = function()
+-- 		require("which-key").setup()
+-- 	end,
+-- }
+
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	config = function()
-		require("which-key").setup()
-	end,
+	opts = {
+		-- preset = "classic",
+		preset = "modern",
+		-- preset = "helix",
+		-- icons = {
+		-- 	rules = {
+		-- 		{ plugin = "laravel.nvim", icon = " ", color = "red" },
+		-- 		{ plugin = "nvim-dap-ui", icon = " ", color = "green" },
+		-- 		{ pattern = "obsidian", icon = " ", color = "blue" },
+		-- 	},
+		-- },
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
+
+
